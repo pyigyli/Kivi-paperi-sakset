4ba5ac9911d5317192c80f230c9a395e23b78b49
pyigyli <pyry.vahakangas@luukku.com>
2018-11-14 21:45:10 +0200

    Herokun toiminnallisuuden korjaus

--- application/models/user.py
+++ application/models/user.py
@@ -53,7 +53,7 @@ class User(db.Model):
         return True
 
     def has_team(self):
-        return self.team_id is not None
+        return self.team_id > 0
 
     def is_anonymous(self):
         return False
