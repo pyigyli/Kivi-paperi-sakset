bb309e4c6f000b218e6922d4a8704680e235ec1e
pyigyli <pyry.vahakangas@luukku.com>
2018-11-20 15:14:45 +0200

    Objektin poistaminen poistaa nyt objektista riippuvat elementit

--- application/views/auth.py
+++ application/views/auth.py
@@ -2,6 +2,9 @@ from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required, current_user
 from application import app, db
 from application.models.user import User
+from application.models.team import Team
+from application.models.comment import Comment
+from application.models.result import Result
 from application.views.forms import CreateUserForm, LoginForm
 
 @app.route("/auth/new", methods = ["GET", "POST"])
@@ -42,6 +45,22 @@ def auth_logout():
 def auth_delete():
     user = User.query.get(current_user.get_id())
     logout_user()
+    team = Team.query.filter_by(creator=user.account_id).first()
+    if team is not None:
+        comments = Comment.query.filter_by(team_id=team.team_id)
+        results = Result.query.filter_by(account_id=user.account_id)
+        for r in results:
+            db.session.delete(r)
+        for c in comments:
+            db.session.delete(c)
+        users = User.query.filter_by(team_id=team.team_id)
+        for u in users:
+            u.team_id = None
+        db.session.delete(team)
+    else:
+        comments = Comment.query.filter_by(account_id=user.account_id)
+        for c in comments:
+            db.session.delete(c)
     db.session().delete(user)
     db.session.commit()
     return redirect(url_for("index"))
