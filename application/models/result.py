61f5fa47fb4a701a440cf9a65bed9a3240525b49
pyigyli <pyry.vahakangas@luukku.com>
2018-11-24 00:42:00 +0200

    Herokun toiminnallisuuksien korjaus

--- application/models/result.py
+++ application/models/result.py
@@ -53,12 +53,9 @@ class Result(db.Model):
                     "FROM account, result "
                     "WHERE account.account_id = result.account_id "
                     "GROUP BY account.account_id "
-                    "ORDER BY lenght(CAST((SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END)) + 0) * 100 AS int)) DESC, "
-                    "(SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END)) + 0) * 100 DESC "
+                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) * 100 ASC "
                     "LIMIT 10;")
         res = db.engine.execute(stmt)
         response = []
@@ -76,9 +73,9 @@ class Result(db.Model):
                     "WHERE team.team_id = account.team_id "
                     "AND account.account_id = result.account_id "
                     "GROUP BY team.team_id "
-                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + 0 "
-                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END)) + 0) * 100 DESC "
+                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) * 100 ASC "
                     "LIMIT 10;")
         res = db.engine.execute(stmt)
         response = []
