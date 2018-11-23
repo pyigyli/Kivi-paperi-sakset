098b4929b70ff1423beed7b9cd730bb5e5ec1a6b
pyigyli <pyry.vahakangas@luukku.com>
2018-11-24 00:17:35 +0200

    Herokun toiminnallisuuksien korjaus

--- application/models/result.py
+++ application/models/result.py
@@ -53,9 +53,12 @@ class Result(db.Model):
                     "FROM account, result "
                     "WHERE account.account_id = result.account_id "
                     "GROUP BY account.account_id "
-                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "ORDER BY len(SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
                     "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + "
-                    "SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) "
+                    "SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))), "
+                    "(SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + "
+                    "SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) DESC"
                     "LIMIT 10;")
         res = db.engine.execute(stmt)
         response = []
