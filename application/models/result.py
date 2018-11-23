4fe256f60e3c7052f39784c1cfa254d3fe9ab64a
pyigyli <pyry.vahakangas@luukku.com>
2018-11-24 00:15:19 +0200

    Herokun toiminnallisuuksien korjaus

--- application/models/result.py
+++ application/models/result.py
@@ -53,9 +53,9 @@ class Result(db.Model):
                     "FROM account, result "
                     "WHERE account.account_id = result.account_id "
                     "GROUP BY account.account_id "
-                    "ORDER BY CAST(ROUND((SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
                     "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) + "
-                    "SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))), 9) AS float) DESC "
+                    "SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) "
                     "LIMIT 10;")
         res = db.engine.execute(stmt)
         response = []
@@ -73,9 +73,9 @@ class Result(db.Model):
                     "WHERE team.team_id = account.team_id "
                     "AND account.account_id = result.account_id "
                     "GROUP BY team.team_id "
-                    "ORDER BY CAST(ROUND((SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
+                    "ORDER BY (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
                     "/ (SUM(CASE WHEN result.winner = 2 THEN 1 ELSE 0 END) "
-                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))), 9) AS float) DESC "
+                    "+ SUM(CASE WHEN result.winner = 0 THEN 1 ELSE 0 END))) DESC "
                     "LIMIT 10;")
         res = db.engine.execute(stmt)
         response = []
